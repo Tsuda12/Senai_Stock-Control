83059f7857d452d09ab8735a27c4dcce3c09ad60
Tsuda12 <felipe.tsuda01@gmail.com>
2022-06-09 06:38:43 -0300

    commit

--- src/tsuda/scripts/Adicionar.java
+++ src/tsuda/scripts/Adicionar.java
@@ -1,10 +1,12 @@
 package tsuda.scripts;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.PrimitiveIterator;
 
-public class Adicionar extends JFrame {
+public class Adicionar extends JFrame implements ActionListener {
     //VARIÁVEIS
     public ArrayList<Produto> produto_lista = new ArrayList<Produto>();
 
@@ -12,6 +14,78 @@ public class Adicionar extends JFrame {
     private JLabel quantidade = new JLabel("Quantidade: ");
     private JTextField nomeProduto = new JTextField(100);
     private JTextField quantidadeProduto = new JTextField(100);
+    private JButton addBotao = new JButton("ADICIONAR");
 
-    //Parou 5:25
+
+    //MÉTODOS
+    public Adicionar(ArrayList<Produto> produto_lista)
+    {
+        super("Adicionar Produto");
+
+        this.produto_lista = produto_lista;
+
+        setLayout(null);
+
+        //--Label e JText nome produto
+        nome.setBounds(10, 10, 180, 100);
+        add(nome);
+        nomeProduto.setBounds(200, 50, 200, 20);
+        add(nomeProduto);
+
+        //--Label quantidade produto
+        quantidade.setBounds(10, 80, 250, 100);
+        add(quantidade);
+        quantidadeProduto.setBounds(270, 120, 130, 20);
+        add(quantidadeProduto);
+
+        //--Botão adicionar
+        addBotao.setBounds(160, 270, 100, 40);
+        add(addBotao);
+        addBotao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nomeP, quantidadeP;
+                int ok = 1;
+                boolean quantidadeTrue = true;
+
+                nomeP = nomeProduto.getText();
+                quantidadeP = quantidadeProduto.getText();
+
+                try{
+                    Double num = Double.parseDouble(quantidadeP);
+                }
+                catch(NumberFormatException et){
+                    quantidadeTrue = false;
+                }
+
+                if(quantidadeTrue == false)
+                {
+                    JOptionPane.showMessageDialog(null, "Digite apenas números!");
+                }
+
+                if (quantidadeTrue == true){
+                    for (int i = 0; i < produto_lista.size(); i++) {
+                        if(produto_lista.get(i).getNome().equals(nomeP))
+                        {
+                            JOptionPane.showMessageDialog(null, "Este produto já existe!");
+                            ok=0;
+                        }
+                    }
+                }
+
+                if(ok==1){
+                    produto_lista.add(new Produto(nomeP, quantidadeP));
+                }
+            }
+        });
+
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setSize(450, 400);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- src/tsuda/scripts/Estoque.java
+++ src/tsuda/scripts/Estoque.java
@@ -1,6 +1,8 @@
 package tsuda.scripts;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Estoque extends JFrame
@@ -29,22 +31,69 @@ public class Estoque extends JFrame
         //--Botão adicionar
         adicionarProduto.setBounds(150, 70, 230, 40);
         add(adicionarProduto);
-        adicionarProduto.addActionListener(new AddProduto());
+        adicionarProduto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //--Chama a tela de adicionar
+                Adicionar adicionarTela = new Adicionar(produtos_lista);
+                adicionarTela.setVisible(true);
+            }
+        });
 
         //--Botão ler
         lerProduto.setBounds(150, 70, 230, 40);
         add(lerProduto);
-        lerProduto.addActionListener(new ReadProduto());
+        lerProduto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //--Checa se a lista está vazia para ler
+                if(produtos_lista.isEmpty()==true){
+                    JOptionPane.showMessageDialog(null, "Sem itens cadastrados!");
+                }
+                else{
+                    //--Chama a janela ler
+                    Ler lerTela = new Ler(produtos_lista);
+                    lerTela.setVisible(true);
+                }
+            }
+        });
 
         //--Botão atualizar
         atualizarProduto.setBounds(150, 70, 230, 40);
         add(atualizarProduto);
-        atualizarProduto.addActionListener(new UpdateProduto());
+        atualizarProduto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //--Checa se a lista está vazia
+                if (produtos_lista.isEmpty()==true){
+                    JOptionPane.showMessageDialog(null, "Sem itens cadastrados!");
+                }
+                else{
+                    //--Chamar a janela atualuzar
+                    Atualizar atualizarTela = new Atualizar(produtos_lista);
+                    atualizarTela.setVisible(true);
+                }
+            }
+        });
 
         //--Botão deletar
         deletarProduto.setBounds(150, 70, 230, 40);
         add(deletarProduto);
-        deletarProduto.addActionListener(new DeleteProduto());
+        deletarProduto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //--Checa se a lista está vazia
+                if(produtos_lista.isEmpty()==true)
+                {
+                    JOptionPane.showMessageDialog(null, "Sem itens cadastrados!");
+                }
+                else {
+                    //--Chama a janela deletar
+                    Deletar deletarTela = new Deletar(produtos_lista);
+                    deletarTela.setVisible(true);
+                }
+            }
+        });
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(450, 400);
